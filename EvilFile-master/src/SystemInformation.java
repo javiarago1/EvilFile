3b6e9ab7d49e0f4aa435cee84834c2b60f8cbf0e
Javier <javiaragoneses1@gmail.com>
2022-04-20 13:46:44 +0200

    Update

---
+++ EvilFile-master/src/SystemInformation.java
@@ -0,0 +1,8 @@
+public class SystemInformation {
+
+    public static String tempRoute = "C:\\Users\\"+getSystemName()+"\\Documents\\pruebas\\temp\\";
+
+    public static String getSystemName (){
+        return System.getProperty("user.name");
+    }
+}
