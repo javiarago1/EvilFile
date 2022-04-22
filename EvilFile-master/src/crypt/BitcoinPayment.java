6f1dba6e40fab9ea2efe945f3e5694a0dbd93c15
Javier Aragoneses Fernández <76445432+javiarago1@users.noreply.github.com>
2022-04-22 13:32:30 +0200

    Update

---
+++ EvilFile-master/src/crypt/BitcoinPayment.java
@@ -0,0 +1,8 @@
+package crypt;
+
+public class BitcoinPayment {
+    public static void main(String[] args) {
+        // Start up a basic app using a class that automates some boilerplate. Ensure we always have at least one key.
+
+    }
+}
